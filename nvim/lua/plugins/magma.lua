0cb6f0046a8926d95909865c7239d4c226d4ce3f
gmg <gomide.gmg@gmail.com>
2023-05-01 13:07:46 -0300

    update dotfiles

---
+++ nvim/lua/plugins/magma.lua
@@ -0,0 +1,16 @@
+return {
+  --"vim-scripts/magma-vim",
+  "dccsillag/magma-nvim",
+  cmd = "Magma",
+  keys = {
+    { "<leader>mm", "<cmd>Magma<cr>", desc = "Magma" },
+  },
+  --keys = {
+  --{ "<leader>mi", "<cmd>MagmaInit<CR>", "MagmaInit" },
+  --{ "<leader>ml", "<cmd>MagmaEvaluateLine<CR>", "Magma Evaluate Line" },
+  --{ "<leader>mv", "<cmd>MagmaEvaluateVisual<CR>", "Magma Evaluate Visual" },
+  --{ "<leader>mI", "<cmd>MagmaInterrupt<CR>", "Magma Interrupt" },
+  --{ "<leader>md", "<cmd>MagmaDelete<CR>", "Magma Delete" },
+  --{ "<leader>m0", "<cmd>MagmaEvaluateOperator<CR>", "Magma Evaluate Operator" },
+  --},
+}
